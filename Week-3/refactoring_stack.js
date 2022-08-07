1a35220d3a2657ebf7c5205e195c0d4bc2eb7d5c
Rajesh Velaga <velagarajeshchowdary@gmail.com>
2022-08-07 08:53:36 +0530

    changes related obj, moved inside the function createStack

--- Week-3/refactoring_stack.js
+++ Week-3/refactoring_stack.js
@@ -1,4 +1,7 @@
 function createStack() {
+  var obj = {
+    items: [],
+  };
   return {
     push(item) {
       obj.items.push(item);
@@ -9,14 +12,9 @@ function createStack() {
   };
 }
 
-var obj = {
-    items: [],
-};
-
 const stack = createStack();
 stack.push(10);
 stack.push(9);
 stack.push(5);
 stack.pop(5);
 console.log(stack.items);
-// console.log(obj.items); this has the stack value
