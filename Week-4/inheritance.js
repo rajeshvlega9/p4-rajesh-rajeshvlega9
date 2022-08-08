f1af8e5225dad676600d130ede9653811448ebd9
Rajesh Velaga <velagarajeshchowdary@gmail.com>
2022-08-08 12:01:33 +0530

    Assignment 2 - Inheritance using prototype

---
+++ Week-4/inheritance.js
@@ -0,0 +1,29 @@
+var Person = function() { };
+Person.prototype.initialize = function (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// This as a object directly using this would work in straight forward way
+// let him = {
+//     __proto__: Person.prototype,
+//     teach: function (subject) {
+//         console.log(this.name + ' is now teaching ' + subject);
+//     }
+// }
+
+// now trying with Class Teacher 
+// this would create a class with constructor 
+class Teacher { }
+// assigning the function initialize to Teacher class
+Teacher.prototype.initialize = Person.prototype.initialize;
+// creating method teach for teacher.
+Teacher.prototype.teach = function (subject) {
+    console.log(`${this.name} is now teaching ${subject}`);
+}
+
+// creating the him object of Teacher class.
+var him = new Teacher();
+
+him.initialize("Adam", 45);
+him.teach("Inheritance");
