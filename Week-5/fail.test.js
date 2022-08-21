051215c19e7114092133b5cd0d6a37ec5c78ad18
Rajesh Velaga <velagarajeshchowdary@gmail.com>
2022-08-21 15:09:13 +0530

    adding the test case for the fail cases for passing one parameter for each function

--- Week-5/fail.test.js
+++ Week-5/fail.test.js
@@ -10,4 +10,13 @@ describe("Maths operation test addition, diff, product", () => {
   test("Product of 3 and 2 should be 6", () => {
     expect(mathOperations.product(3, 2)).toBe(2);
   });
+  test("adding only 4 should be NaN", () => {
+    expect(mathOperations.sum(4)).toBe(33);
+  });
+  test("passing only 3 should be NaN", () => {
+    expect(mathOperations.diff(3)).toBe(1);
+  });
+  test("Product of only 3 should be NaN ", () => {
+    expect(mathOperations.product(3)).toBe(2);
+  });
 });
