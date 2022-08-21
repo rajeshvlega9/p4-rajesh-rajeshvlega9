9684740c960e4d6f4e38003a44fe3458cfa4f472
Rajesh Velaga <velagarajeshchowdary@gmail.com>
2022-08-21 14:44:54 +0530

    adding seperate file for generators and asyncawait

--- Week-5/asyncawait.js
+++ Week-5/asyncawait.js
@@ -20,23 +20,4 @@ async function asyncFn() {
   console.log("------------------------------------------------\n");
 }
 
-function* genFn() {
-  console.log("---------- Generator - function call ---------- \n");
-  const no_1 = doTask1(2);
-  yield no_1;
-
-  const n1_1 = doTask2(no_1);
-  yield n1_1;
-
-  const n2_1 = doTask3(n1_1);
-  yield n2_1;
-
-  console.log("------------------------------------------------\n");
-}
-
-const genObj = genFn();
-console.log(`first no - ${genObj.next().value}`);
-console.log(`second no - ${genObj.next().value}`);
-console.log(`Magic no - ${genObj.next().value}`);
-
 asyncFn();
