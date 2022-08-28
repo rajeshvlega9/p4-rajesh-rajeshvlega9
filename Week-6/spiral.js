d8a307e4061fa0b2e0d6d25a737346f7e6451d17
Rajesh Velaga <velagarajeshchowdary@gmail.com>
2022-08-28 15:27:39 +0530

    Assignment on Spiral order for the 2D arrays

---
+++ Week-6/spiral.js
@@ -0,0 +1,73 @@
+function matrix(m, n) {
+  var num = 1;
+  var arrX = [];
+  var arrY = [];
+  for (let i = 0; i < m; i++) {
+    arrY = [];
+    for (let j = 0; j < n; j++) {
+      arrY[j] = num;
+      num++;
+    }
+    arrX[i] = arrY;
+  }
+  return arrX;
+}
+
+function spiralOrder(matrix) {
+  var numbers = [];
+  let k = 0;
+  let i = 0;
+  let j = 0;
+  var temp_i = 0;
+  let num_i = matrix.length - 1;
+  let num_j = 0;
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+      //first line print as it
+      if (i === 0) {
+        numbers[k] = matrix[i][j];
+        k++;
+        num_j = j; //capture for further reference
+      }
+    }
+    // traverse vertically until before last line.
+    temp_i = i + 1;
+    while (temp_i <= num_i - 1 && temp_i > 0) {
+      num_i--;
+      numbers[k] = matrix[temp_i][num_j];
+      temp_i++;
+      k++;
+    }
+    //Last line print reverse
+    if (i === matrix.length - 1) {
+      for (j = matrix[i].length - 1; j >= 0; j--) {
+        numbers[k] = matrix[i][j];
+        k++;
+      }
+      temp_i = i - 1;
+    }
+  }
+  num_j--;
+  j = 0;
+  while (j <= num_j) {
+    numbers[k] = matrix[temp_i][j];
+    k++;
+    j++;
+  }
+  return numbers;
+}
+
+var mat33 = matrix(3, 3);
+console.log("Matrix of 3,3 -> \n");
+console.log(mat33);
+console.log("\n");
+console.log("Spiral Order for matrix 3,3 -> \n");
+console.log(spiralOrder(mat33));
+
+
+var mat34 = matrix(3, 4);
+console.log("Matrix of 3,4 -> \n");
+console.log(mat34);
+console.log("\n");
+console.log("Spiral Order for matrix 3,4 -> \n");
+console.log(spiralOrder(mat34));
