b92d3dc624828ce8770d3ba0fe3ad41cf47e793e
Rajesh Velaga <velagarajeshchowdary@gmail.com>
2022-11-02 19:54:20 +0530

    changes made to forecast to render

--- Week-13/Weather/app.js
+++ Week-13/Weather/app.js
@@ -30,69 +30,115 @@ app.get('/current', (req, res) => {
 })
 
 app.get('/forecast', (req, res) => {
-    res.render('forecast');
+    res.render('forecast', {location:null, lineItems:null ,error:null});
 })
 
 app.post('/current', (req, res) => {
     
     let city = req.body.city;
     // let city = 'Pune';
-    if(city) {
-    let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
-    console.log(city);
-    request(url, (err, request, body) => {
-        if (err) {
-            let error = "Error" + err;
-            res.render('current', {
-                city: null,
-                temp_c: null,
-                error: err
-            });
-        } else {
-            console.log(body);
-            let current_weather = JSON.parse(body);
-            let error = JSON.parse(body);
-            if (current_weather.location === undefined) {
-                res.render('current',
-                    {
-                        city: null,
-                        region: null,
-                        localtime: null,
-                        localtime_zone: null,
-                        temp_c: null,
-                        temp_f: null,
-                        icon: null,
-                        cond_text: null,
-                        wind_speed_kph: null,
-                        wind_dir:null,
-                        error: current_weather.error.message
-                    })
-            } else {
-                console.log(current_weather);
+    if (city) {
+        let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+        console.log(city);
+        request(url, (err, request, body) => {
+            if (err) {
+                let error = "Error" + err;
                 res.render('current', {
-                    city: current_weather.location.name,
-                    region: current_weather.location.region + ', ' + current_weather.location.country,
-                    localtime: current_weather.location.localtime,
-                    localtime_zone: current_weather.location.tz_id,
-                    temp_c: current_weather.current.temp_c,
-                    temp_f: current_weather.current.temp_f,
-                    cond_text: current_weather.current.condition.text,
-                    icon: current_weather.current.condition.icon,
-                    wind_speed_kph: current_weather.current.wind_kph,
-                    wind_dir: current_weather.current.wind_dir,
-                    feelslike_c: current_weather.current.feelslike_c,
-                    error: null
+                    city: null,
+                    temp_c: null,
+                    error: err
                 });
+            } else {
+                console.log(body);
+                let current_weather = JSON.parse(body);
+           
+                if (current_weather.location === undefined) {
+                    res.render('current',
+                        {
+                            city: null,
+                            region: null,
+                            localtime: null,
+                            localtime_zone: null,
+                            temp_c: null,
+                            temp_f: null,
+                            icon: null,
+                            cond_text: null,
+                            wind_speed_kph: null,
+                            wind_dir: null,
+                            error: current_weather.error.message
+                        })
+                } else {
+                    console.log(current_weather);
+                    res.render('current', {
+                        city: current_weather.location.name,
+                        region: current_weather.location.region + ', ' + current_weather.location.country,
+                        localtime: current_weather.location.localtime,
+                        localtime_zone: current_weather.location.tz_id,
+                        temp_c: current_weather.current.temp_c,
+                        temp_f: current_weather.current.temp_f,
+                        cond_text: current_weather.current.condition.text,
+                        icon: current_weather.current.condition.icon,
+                        wind_speed_kph: current_weather.current.wind_kph,
+                        wind_dir: current_weather.current.wind_dir,
+                        feelslike_c: current_weather.current.feelslike_c,
+                        error: null
+                    });
+                }
             }
-        }
-    })
+        })
     }
     // city = '';
 })
 
 app.post('/forecast', (req, res) => {
-    let city = req.body.city;
+    let place  = req.body.place;
     let period = req.body.period;
+    let lineItems = [];
+
+    console.log(place);
+    console.log(period);
+
+    let url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${place}&days=${period}`
+
+   request(url, (err, request, body) => {
+        // console.log(body/);
+        var forecast = JSON.parse(body);
+        var forecasts = forecast.forecast;
+
+        if (forecasts.forecastday === undefined) {
+            res.render('forecast', {
+                location: null,
+                lineItems: null,
+                error: "No records to show due to error"
+            });
+        } else {
+
+            var location = forecast.location;
+            // let forecastDate = forecasts.forecastday[0].date;
+            // let forecastDay = forecasts.forecastday[0].day;
+            var forecastDayItems = forecasts.forecastday;
+
+            // console.log(forecast);
+            // console.log(forecasts);
+            // console.log(forecastDate);
+            // console.log(forecastDay);
+            console.log(location);
+            lineItems.push(forecastDayItems);
+            lineItems.forEach((item, ind) => {
+                // console.log(item);
+                item.forEach((it, id) => {
+                    console.log(it.date);
+                    console.log(it.day);
+                });
+            });
+
+            res.render('forecast', {
+                location: location,
+                lineItems: lineItems,
+                error: null
+            });
+        }
+    });
 
 })
 
@@ -100,25 +146,25 @@ app.use((req, res) => {
     res.status(404).render('404');
 })
 
-const get_current_weather = (city) => {
-    let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+// const get_current_weather = (city) => {
+//     let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
 
-    request(url, (err, request, body) => {
-        if (err) {
-            let error = "Error" + err;
+//     request(url, (err, request, body) => {
+//         if (err) {
+//             let error = "Error" + err;
      
-        } else {
-            let current_weather = JSON.parse(body);
-            console.log(current_weather);
-            // weather.
-            // res.render('current', {weather:weather,error:null});
-        }
-    })
+//         } else {
+//             let current_weather = JSON.parse(body);
+//             console.log(current_weather);
+//             // weather.
+//             // res.render('current', {weather:weather,error:null});
+//         }
+//     })
 
-};
+// };
 
-const get_forecast_city = (city, days) => {
+// const get_forecast_city = (city, days) => {
     
-}
+// }
 
 // get_current_weather('Bengaluru');
